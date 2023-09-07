9f740392017e38f60f3eccc99a2b7e357ba85b86
Emmanuel Patricio <L192909399@cdguzman.tecnm.mx>
2023-09-06 21:00:45 -0600

    Updated IconButtons

--- mcflutterapp/lib/home_page.dart
+++ mcflutterapp/lib/home_page.dart
@@ -1,37 +1,33 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Home_page extends StatefulWidget {
+  const Home_page({super.key, required String title});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Mc Flutter'),
-    );
+  State<Home_page> createState() => HomePageState();
+}
+
+class HomePageState extends State<Home_page> {
+  final List<Color> _colorIcon = [
+    Colors.black,
+    Colors.black,
+    Colors.black,
+    Colors.black,
+  ];
+
+  void _switchColor(int index) {
+    setState(() {
+      _colorIcon[index] =
+          _colorIcon[index] == Colors.indigo ? Colors.black : Colors.indigo;
+    });
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Colors.amberAccent,
+        title: const Text("Mc Flutter"),
       ),
       body: Container(
         width: 400,
@@ -42,10 +38,10 @@ class _MyHomePageState extends State<MyHomePage> {
           width: 2,
           color: Colors.grey,
         )),
-        child: const Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            ListTile(
+            const ListTile(
               leading: Padding(
                 padding: EdgeInsets.only(left: 35),
                 child: Icon(
@@ -56,25 +52,87 @@ class _MyHomePageState extends State<MyHomePage> {
               title: Text("Flutter McFLutter"),
               subtitle: Text("Experienced App Developer"),
             ),
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Text("123 Main Street"), Text("(420) 555-0198")],
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text("123 Main Street")),
+                Padding(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Text("(420) 555-0198"),
+                )
+              ],
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                SizedBox(
+                const SizedBox(
                   width: 5,
                 ),
-                Icon(Icons.accessibility_new_rounded),
-                Icon(Icons.alarm),
-                Icon(Icons.phone_android),
-                Icon(Icons.add_alert_rounded),
-                SizedBox(
-                  width: 5,
+                IconButton(
+                  icon: const Icon(Icons.accessibility_new_rounded),
+                  color: _colorIcon[0],
+                  onPressed: () {
+                    setState(() {
+                      _switchColor(0);
+                      const snackBarA = SnackBar(
+                        content: Text('Acceso'),
+                        backgroundColor: (Colors.blueGrey),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBarA);
+                    });
+                  },
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _switchColor(1);
+                      const snackBarAA = SnackBar(
+                        content: Text('Agregar Alarma'),
+                        backgroundColor: (Colors.blueGrey),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBarAA);
+                    });
+                  },
+                  icon: const Icon(Icons.alarm_add_rounded),
+                  color: _colorIcon[1],
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _switchColor(2);
+                      const snackBarL = SnackBar(
+                        content: Text('Llamada'),
+                        backgroundColor: (Colors.blueGrey),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBarL);
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.phone_android,
+                  ),
+                  color: _colorIcon[2],
+                ),
+                IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _switchColor(3);
+                      const snackBarN = SnackBar(
+                        content: Text('Notificaciones Activadas'),
+                        backgroundColor: (Colors.blueGrey),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBarN);
+                    });
+                  },
+                  icon: const Icon(Icons.add_alert_rounded),
+                  color: _colorIcon[3],
+                ),
+                const SizedBox(
+                  width: 20,
                 )
               ],
             ),
@@ -82,5 +140,6 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+    ;
   }
 }
--- mcflutterapp/lib/main.dart
+++ mcflutterapp/lib/main.dart
@@ -2,3 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:mcflutterapp/home_page.dart';
 
 void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
+      ),
+      home: const Home_page(
+        title: 'Mc Flutter',
+      ),
+    );
+  }
+}
